f8dfb8684ba3c478af98f130e8247743ec0d70e6
sat.open.world <sat.open.world@gmail.com>
2025-11-19 17:51:51 +0000

    ы

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -1,10 +1,20 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
 
 export const ThemeSwitcher = () => {
+    const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
 
     return (
         <button
