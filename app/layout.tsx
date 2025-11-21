ccf389aa2017d160f66644ae20ce9554b9bed156
sat.open.world <sat.open.world@gmail.com>
2025-11-21 17:17:16 +0000

    иконки

--- app/layout.tsx
+++ app/layout.tsx
@@ -125,8 +125,10 @@ export default function RootLayout({
           content="black-translucent"
         />
         <meta name="apple-mobile-web-app-title" content="Дарья Калашникова" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${inter.className} antialiased`}>
         <Providers>
